f982b1ef59dc75d1336710f6e041407b3d02b22a
Christopher Peret <cperet89@gmail.com>
2023-02-18 21:30:54 -0800

    created files and directories. started script.js

---
+++ develop/script.js
@@ -0,0 +1,11 @@
+// This is a blackjack program
+// planned card counting option
+// single player against dealer
+// user can select number of decks to play with
+// user can select number of players
+// players abide by basic strategy
+
+//card constants
+const suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades'];
+const values = ['Ace', 'King', 'Queen', 'Jack', 'Ten', 'Nine', 'Eight', 'Seven', 'Six', 'Five', 'Four', 'Three', 'Two'];
+
