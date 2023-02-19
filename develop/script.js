6956fdee811004f7e59288a1372dae09160d834a
Christopher Peret <cperet89@gmail.com>
2023-02-19 14:18:13 -0800

    created objects for card properties

--- develop/script.js
+++ develop/script.js
@@ -6,9 +6,6 @@
 // user can select number of players
 // players abide by basic strategy
 
-//card constants
-const suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades'];
-const values = ['Ace', 'King', 'Queen', 'Jack', 'Ten', 'Nine', 'Eight', 'Seven', 'Six', 'Five', 'Four', 'Three', 'Two'];
 
 //DOM variables and event listeners
 let textArea = document.getElementById('text-area');
@@ -26,22 +23,44 @@ const resetButton = document.getElementById('reset-button');
 resetButton.addEventListener('click', reset);
 
 
+//card constants
+const suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades'];
+const values = ['Ace', 'King', 'Queen', 'Jack', 'Ten', 'Nine', 'Eight', 'Seven', 'Six', 'Five', 'Four', 'Three', 'Two'];
 
 //Game variables
+
 let startGame = false,
     gameOver = false,
     playerWon = false,
+    deck = [],
+    shuffledDeck = [],
     dealerCards = [],
     playerCards = [],
+    discardPile = [],
     dealerScore = 0,
     playerScore = 0,
-    deck = [],
     numDecks = 0,
     numPlayers = 0,
     players = [];
 
 //card objects
-function Player() {
-    .cards = [];
-    this.score = 0;
-}
+// Define card objects using object literals
+const playerCard = {
+    suit: '',
+    value: '' 
+};
+
+const dealerCard = {
+    suit: '',
+    value: ''
+};
+
+const discardPile = {
+    suit: '',
+    value: '' 
+};
+
+const playerHand = {
+  cards: [],
+  score: 0
+};
