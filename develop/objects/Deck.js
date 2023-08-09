9fb2f9a773469eea455908dda30cdf7d88183218
Christopher Peret <cperet89@gmail.com>
2023-08-09 12:51:15 -0700

    Deck class revision

--- develop/objects/Deck.js
+++ develop/objects/Deck.js
@@ -6,11 +6,37 @@ class Deck {
         this.suits = ['Clubs', 'Diamonds', 'Hearts', 'Spades'];
         this.values = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
 
-        for (let suit of this.suits) { // for each suit
-            for (let value of this.values) { // for each value
-                this.cards.push(new Card(suit, value)); // create a new card and add it to the deck
+        this.populate();
+    }
+
+    populate() {
+        this.cards.length = 0;  // Empty the array
+        for (let suit of this.suits) {
+            for (let value of this.values) {
+                this.cards.push(new Card(suit, value));
             }
         }
+    }
+
+    shuffle() {
+        for (let i = this.cards.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]]; // Swap cards
+        }
+    }
+
+    deal() {
+        if (this.cards.length > 0) {
+            return this.cards.pop();
+        } else {
+            console.error('The deck is empty! Cannot deal.');
+            return null;
+        }
+    }
+
+    reset() {
+        this.populate();
+        this.shuffle();
     }
 }
 
