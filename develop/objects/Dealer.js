1973d51f2cc41b2eb712190fa4a6ee2174ac34b9
Christopher <123341169+Zed-CSP@users.noreply.github.com>
2023-08-16 15:36:58 -0700

    Update Dealer.js

--- develop/objects/Dealer.js
+++ develop/objects/Dealer.js
@@ -1,16 +1,41 @@
-import Participant from './Participant.js';
-
+import Participant from './Participant.js';  
 class Dealer extends Participant {
     constructor() {
         super();
     }
-    
-    get isShowingFace() {
-        return this.hand.cards[0].cardvalue == 10 || this.hand.cards[0].value == 'Ace';
+
+    // Dealer's rule for hitting or standing
+    shouldHit() {
+        return this.primaryHand.handValue < 17 || (this.primaryHand.handValue === 17 && this.primaryHand.isSoft);
     }
 
-    showcards() {
-        this.hand.cards.faceUp = true;
+    // Deal the initial hand to the dealer
+    initialDeal(card1, card2) {
+        card1.faceUp = true;  // The first card is face-up
+        card2.faceUp = false; // The second card is face-down
+        this.primaryHand.cards.push(card1, card2);
+    }
+
+    // Reveal the face-down card
+    revealHiddenCard() {
+        for (let card of this.primaryHand.cards) {
+            card.faceUp = true;
+        }
+    }
+
+    // Check if dealer has a blackjack
+    get hasBlackjack() {
+        return this.primaryHand.isBlackjack;
+    }
+
+    // Check if dealer's hand is a bust
+    get isBust() {
+        return this.primaryHand.isBust;
+    }
+
+    // Check if dealer's hand is soft
+    get isSoft() {
+        return this.primaryHand.isSoft;
     }
 }
 
